e51cd969c586f3e1daad8f52fdc3405e4de5bbe6
eteced <eteced@gmail.com>
2023-07-02 00:53:04 +0800

    init commit from llama adapter

---
+++ finetuning.sh
@@ -0,0 +1,13 @@
+torchrun --nproc_per_node 1 finetuning.py \
+    --model Llama7B_adapter \
+    --llama_model_path /home/eteced/dl_workspace/model_repo.folder/llama_ord/ \
+    --data_path /home/eteced/dl_workspace/stanford_alpaca/alpaca_data.json \
+    --adapter_layer 30 \
+    --adapter_len 10 \
+    --max_seq_len 512 \
+    --batch_size 1 \
+    --epochs 5 \
+    --warmup_epochs 2 \
+    --blr 9e-3 \
+    --weight_decay 0.02 \
+    --output_dir ./checkpoint/
